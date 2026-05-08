bbac5a0e3bb29040aa442190fa14d5e1de6c2cc1
David Cardenas Lorca <cine.dcl@gmail.com>
2026-05-07 22:33:21 -0400

    feat(exceptions): lift HTTP exception handlers from starters

---
+++ src/Exceptions/TooManyRequestsException.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dcardenasl\Ci4ApiCore\Exceptions;
+
+use dcardenasl\Ci4ApiCore\Exceptions\ApiException;
+
+/**
+ * Too Many Requests Exception (HTTP 429)
+ *
+ * Thrown when the rate limit has been exceeded.
+ * Examples:
+ * - API rate limit exceeded
+ * - Login attempts exceeded
+ * - Email sending limit reached
+ */
+class TooManyRequestsException extends ApiException
+{
+    /**
+     * HTTP status code
+     *
+     * @var int
+     */
+    protected int $statusCode = 429;
+
+    /**
+     * Constructor
+     *
+     * @param string|null $message Error message (default: lang('Exceptions.tooManyRequests'))
+     * @param array<string, string|list<string>> $errors Structured error details
+     */
+    public function __construct(?string $message = null, array $errors = [])
+    {
+        parent::__construct($message ?? lang('Exceptions.tooManyRequests'), $errors);
+    }
+}
